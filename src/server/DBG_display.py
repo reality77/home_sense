77d21a7802297436b608dde57787b709b0baf777
reality77 <davmail08-git@yahoo.fr>
2016-03-14 15:17:12 +0100

    [LED_DISPLAY] Corrected hexa address

--- src/server/DBG_display.py
+++ src/server/DBG_display.py
@@ -7,7 +7,7 @@ class Display:
   disp = None
 
   def __init__(self, address):
-    print "DISPLAY : set address 0x{0}".format(address)
+    print "DISPLAY : set address 0x{0:x}".format(address)
 
   def writeChar(self, charNumber, value):
     print "DISPLAY : set char {0} value : {1}".format(charNumber, value)
